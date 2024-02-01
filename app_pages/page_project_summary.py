e9bab084f22fa72e36c71f3a0e4ca85c97529086
0w3nM <>
2024-02-01 09:11:31 +0000

    Updated project summary

--- app_pages/page_project_summary.py
+++ app_pages/page_project_summary.py
@@ -9,8 +9,8 @@ def page_summary_body():
     st.info(
         f"**Project Dataset**\n"
         f"The dataset is representing a list of **houses**, in Ames, Iowa"
-        f"focusing on their attributes. Floor Area, Basement, Garage,
-        f"Kitchen, Lot, Porch, Wood Deck and Year Built,"
+        f"focusing on their attributes. Floor Area, Basement, Garage," 
+        f"Kitchen, Lot, Porch, Wood Deck and Year Built"
         f"with houses sale price between the years 1872 & 2010.")
 
     # Link to README file, so the users can have access to full project documentation
