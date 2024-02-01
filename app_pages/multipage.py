e352c8084651934bc6f579c1d91258b5754efdee
0w3nM <>
2024-02-01 09:20:33 +0000

    Updated multipage

--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
-# load pages scripts
-from app_pages.page_project_summary import page_project_summary_body
-from app_pages.page_sale_price_correlation_analysis import page_sale_price_correlation_analysis_body
-from app_pages.page_project_price_predictor import page_project_price_predictor_body
-from app_pages.page_project_hypothesis import page_project_hypothesis_body
-from app_pages.page_project_ml_model import page_project_ml_model_body
+class MultiPage:
 
-app = MultiPage(app_name="Heritage Housing Predictor")
+    def __init__(self, app_name) -> None:
+        self.pages = []
+        self.app_name = app_name
 
-# Add your app pages here using .add_page()
-app.add_page("Project Summary", page_project_summary_body)
-app.add_page("Price Correlation Analysis", page_sale_price_correlation_analysis_body)
-app.add_page("Price Predictor", page_project_price_predictor_body)
-app.add_page("Project Hypothesis", page_project_hypothesis_body)
-app.add_page("ML: Model", page_project_ml_model_body)
+        st.set_page_config(
+            page_title=self.app_name,) 
 
-app.run()
+    def add_page(self, title, func) -> None:
+        self.pages.append({"title": title, "function": func})
+
+    def run(self):
+        st.title(self.app_name)
+        page = st.sidebar.radio('Menu', self.pages,
+                                format_func=lambda page: page['title'])
+        page['function']()
