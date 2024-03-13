2bfa892254dae02687b65b682a253ba6f227ddac
0w3nM <mickeyo_1@hotmail.co.uk>
2024-03-13 11:44:43 +0000

    Added code to create a dataframe with the inherited house data.

--- src/data_management.py
+++ src/data_management.py
@@ -9,6 +9,10 @@ def load_house_data():
     df = pd.read_csv("outputs/datasets/collection/house_prices_records.csv")
     return df
 
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def load_inherited_houses_data():
+    df = pd.read_csv("inputs/datasets/raw/house-price-20211124T154130Z-001/house-price/inherited_houses.csv")
+    return df
 
 def load_pkl_file(file_path):
     return joblib.load(filename=file_path)
